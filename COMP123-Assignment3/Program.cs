40463b9ad0e2ea454c35e19cc3158f1d865ddf40
oreeba <oreebabadar@gmail.com>
2016-07-08 01:28:06 -0400

    completed all tasks of assignment, missing internal documentation

--- COMP123-Assignment3/Program.cs
+++ COMP123-Assignment3/Program.cs
@@ -11,6 +11,27 @@ namespace COMP123_Assignment3
         static void Main(string[] args)
 
         {
+
+            Console.WriteLine("        GIANT PLANET: ");
+            GiantPlanet giantPlanet = new GiantPlanet("        Gigantous", 1504199710, 0179914051, "Ice");
+            Console.WriteLine("--------------------------------------");
+            Console.WriteLine(giantPlanet.ToString());
+
+            Console.WriteLine("*************************************");
+            Console.WriteLine("        \n        TERRESTRIAL PLANET: ");
+            TerrestrialPlanet terrestrialPlanet = new TerrestrialPlanet("        Infinitesimal", 101998, 899101, true);
+            Console.WriteLine("--------------------------------------");
+
+            Console.WriteLine(terrestrialPlanet.ToString());
+            Console.WriteLine("*************************************");
+            waitForAnyKey();
+        }
+
+        public static void waitForAnyKey()
+        {
+            Console.WriteLine("Press any key to continue ");
+            Console.ReadKey();
         }
     }
-}
+    }
+
